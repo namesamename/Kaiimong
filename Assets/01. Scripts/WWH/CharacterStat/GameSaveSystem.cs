d17e753311360892d42f8668f1a186e475ffff0c
woomay0608 <woohuck0608@naver.com>
2025-04-09 20:31:54 +0900

    [feat] 저장 구현

--- Assets/01. Scripts/WWH/CharacterStat/GameSaveSystem.cs
+++ Assets/01. Scripts/WWH/CharacterStat/GameSaveSystem.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
+using System.IO;
+using static CharacterDataBase;
 
 public enum SaveType
 {
@@ -11,62 +13,57 @@ public enum SaveType
 public class GameSaveSystem : Singleton<GameSaveSystem>
 {
 
-    public readonly string CharacterKey = "Character";
+    
 
-    public Dictionary<int, List<SaveInstance>> SaveDic = new Dictionary<int, List<SaveInstance>>();
+    public Dictionary<SaveType, List<SaveInstance>> SaveDic = new Dictionary<SaveType, List<SaveInstance>>();
 
     
     public void SaveData(List<SaveInstance> saves)
     {
         if (saves[0].Savetype == SaveType.Character)
         {
-            SaveDataToEnum(1, saves, CharacterKey);
+            SaveDataToEnum( SaveType.Character ,saves);
         }
       
     }
 
-    public void SaveDataToEnum(int index, List<SaveInstance> saves, string Key)
+    public void SaveDataToEnum( SaveType saveType ,List<SaveInstance> saves)
     {
-        SaveDic[index] = saves;
-        string json = JsonConvert.SerializeObject(new SaveDataWrapper { Saves = SaveDic[index] }, Formatting.Indented
+        SaveDic[saveType] = saves;
+
+        string path = Path.Combine(Application.dataPath, $"JsonData/{saveType}/{saveType}Database.json");
+
+        string json = JsonConvert.SerializeObject(new SaveDataWrapper { Saves = SaveDic[SaveType.Character] }, Formatting.Indented
             , new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto });
-        PlayerPrefs.SetString(Key, json);
-        PlayerPrefs.Save();
+
+        File.WriteAllText(path, json);
         
     }
 
-    public List<SaveInstance> Load(int index, string Key)
+    public List<SaveInstance> Load(SaveType saveType)
     {
-        if(PlayerPrefs.GetString(Key) == string.Empty)
+        string path = Path.Combine(Application.dataPath, $"JsonData/{saveType}/{saveType}Database.json");
+        if(File.Exists(path))
         {
-            Debug.LogError("Data Do not Exist");
+            string json =  File.ReadAllText(path);
+            List<SaveInstance> saves = new List<SaveInstance>();
+            SaveDataWrapper saveData = JsonConvert.DeserializeObject<SaveDataWrapper>
+            (json, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto});
+            saves = saveData.Saves;
+            return saves;
         }
-        else if(!PlayerPrefs.HasKey(Key))
+        else
         {
-            Debug.LogError("Data Do not Exist");
-        }
-
-        Debug.Log("Load");
-
-
-        List<SaveInstance> saves = new List<SaveInstance>();
-        SaveDataWrapper saveData = JsonConvert.DeserializeObject<SaveDataWrapper>
-            (PlayerPrefs.GetString(Key), new JsonSerializerSettings
-        {
-            TypeNameHandling = TypeNameHandling.Auto
-        });
-        saves = saveData.Saves;
-        return saves;
+           return new List<SaveInstance>();
+        } 
     }
-
-
-  
-
-
     [System.Serializable]
     public class SaveDataWrapper
     {
         public List<SaveInstance> Saves = new List<SaveInstance>();
     }
+
+
+
 
 }
