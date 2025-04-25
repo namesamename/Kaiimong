4d2eb77660c63f294c3facf2c7fcc0fd056d83e4
woomay0608 <woohuck0608@naver.com>
2025-04-25 14:53:30 +0900

    [feat] 스태미나 회복창

--- Assets/01. Scripts/UIStaminaChargePOPUP.cs
+++ Assets/01. Scripts/UIStaminaChargePOPUP.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIStaminaChargePOPUP : MonoBehaviour
+public class UIStaminaChargePOPUP : UIBreakPOPUP
 {
     Button[] Buttons;
     Image[] Images;
@@ -15,6 +15,56 @@ public class UIStaminaChargePOPUP : MonoBehaviour
         Buttons = GetComponentsInChildren<Button>();
         Images = GetComponentsInChildren<Image>();
         textMesh = GetComponentsInChildren<TextMeshProUGUI>();
+
+    }
+
+
+    private void Start()
+    {
+        for (int i = 0; i < Buttons.Length; i++) 
+        {
+            Buttons[0].onClick.RemoveAllListeners();
+        }
+
+    }
+
+
+    public void BTNSet()
+    {
+
+        Buttons[0].onClick.AddListener(Destroy);
+        Buttons[1].onClick.AddListener(Destroy);
+        //개별 조건을 달아준다
+        Buttons[2].onClick.AddListener(SmallStaminaUse);
+        Buttons[3].onClick.AddListener(BigStaminaUse);
+        Buttons[4].onClick.AddListener(CrystalStaminaUse);
+    }
+
+    public void ImageSET()
+    {
+        //Images[3].sprite = Resources.Load<Sprite>( GlobalDataTable.Instance.Item.GetItemDataToID(0).IconPath);
+        //Images[4].sprite = Resources.Load<Sprite>(GlobalDataTable.Instance.Item.GetItemDataToID(0).IconPath);
+        //Images[5].sprite = Resources.Load<Sprite>(GlobalDataTable.Instance.currency.GetCurrencySOToEnum<DIaCurrencySO>(CurrencyType.Dia).IconPath);
+
+    }
+
+    public void TextSet()
+    {
+      
+    }
+
+    public void SmallStaminaUse()
+    {
+
+    }
+
+    public void BigStaminaUse()
+    {
+        
+    }
+
+    public void CrystalStaminaUse()
+    {
 
     }
 
