d610f14e8f6eb6a2a0b344765a44391da705ef29
woomay0608 <woohuck0608@naver.com>
2025-04-27 19:46:37 +0900

    [feat] 저장 기능 추가

--- Assets/01. Scripts/EditorWindow/CurrencyEditorWindow.cs
+++ Assets/01. Scripts/EditorWindow/CurrencyEditorWindow.cs
@@ -1,18 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class CurrencyEditorWindow : MonoBehaviour
+public class CurrencyEditorWindow : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool boolValue;
+    private float floatValue;
+    private Vector3 vector3Value;
+
+    [MenuItem("TestWindow/Currency %h")]
+    public static void EditorShow()
     {
-        
+        CurrencyEditorWindow window = GetWindow<CurrencyEditorWindow>();
+        window.titleContent = new GUIContent("Currency");
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    private void OnGUI()
     {
-        
+        if (GUI.Button(new Rect(0, 0, 250, 100), "Creat new SaveData"))
+        {
+            List<SaveInstance> list = new List<SaveInstance>();
+            CurrencySaveData saveData = new CurrencySaveData()
+            {
+                ID = 0,
+                Savetype = SaveType.Currency,
+                ActivityValue = 0,
+                DIAValue = 0,
+                CharacterEXP = 0,
+                GachaValue = 0,
+                CurrentStaminaMax = 0,
+                UserLevel = 0,
+                UserEXP = 0,
+                UserName = string.Empty,
+            };
+            list.Add(saveData);
+            GameSaveSystem.Save(SaveType.Currency, list);
+        }
+
+        // ±½Àº ±Û¾¾ 
+        Color originColor = EditorStyles.boldLabel.normal.textColor;
+        EditorStyles.boldLabel.normal.textColor = Color.yellow;
+
+        // Header =====================================================================
+        GUILayout.Space(10f);
+        GUILayout.Label("Header Label", EditorStyles.boldLabel);
+
+        vector3Value = EditorGUILayout.Vector3Field("Vector3", vector3Value);
+
+        // ============================================================================
+        GUILayout.Space(10f);
+        GUILayout.Label("Horizontal", EditorStyles.boldLabel);
+
+        // Horizontal =================================================================
+        GUILayout.BeginVertical();
+
+        boolValue = EditorGUILayout.Toggle("Bool", boolValue);
+        floatValue = EditorGUILayout.FloatField("Float", floatValue);
+
+        GUILayout.EndVertical();
+
+        // Horizontal =================================================================
+        GUILayout.BeginHorizontal();
+
+        GUILayout.Label("Label Left");
+        GUILayout.Label("Label Right");
+
+        GUILayout.EndHorizontal();
+        // ============================================================================
+
+        EditorStyles.boldLabel.normal.textColor = originColor;
+
+
+
+
     }
+
+
+
 }
