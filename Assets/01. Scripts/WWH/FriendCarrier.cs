d2d22c15316e8171dd1aae3dcb44c94936bb9e73
woomay0608 <woohuck0608@naver.com>
2025-04-27 18:40:56 +0900

    [feat] ID 및 인벤토리 수정

--- Assets/01. Scripts/WWH/FriendCarrier.cs
+++ Assets/01. Scripts/WWH/FriendCarrier.cs
@@ -6,6 +6,7 @@ public class FriendCarrier : CharacterCarrier
 {
     public override void Initialize(int id, int level =-1)
     {
+        SetID(id);
         visual.Initialize(id, CharacterType.Friend);
         stat.SetCharacter(GlobalDataTable.Instance.character.GetCharToID(id), CharacterManager.Instance.GetSaveData(id).Level);
         skillBook.SkillSet(id);
