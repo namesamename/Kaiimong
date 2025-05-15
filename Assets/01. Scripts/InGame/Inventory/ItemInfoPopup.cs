b3b1ac8f158de157dbf12042b4a4fcc735784a53
woomay0608 <woohuck0608@naver.com>
2025-05-15 10:06:44 +0900

    [chore] 분기점

--- Assets/01. Scripts/InGame/Inventory/ItemInfoPopup.cs
+++ Assets/01. Scripts/InGame/Inventory/ItemInfoPopup.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.UI;
-using UnityEngine;
+
 using UnityEngine.UI;
 
 public class ItemInfoPopup : UIPOPUP
