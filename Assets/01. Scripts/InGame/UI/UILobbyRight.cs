cfbd65cbca4eb3273bc4d38818838afee6047377
woomay0608 <woohuck0608@naver.com>
2025-05-16 09:39:16 +0900

    [feat] 추가

---
+++ Assets/01. Scripts/InGame/UI/UILobbyRight.cs
@@ -0,0 +1,15 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILobbyRight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOLocalMoveX(0, 1f);
+    }
+
+
+}
