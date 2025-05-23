9c4b99b0d0e2081e5a9bcf21a0b11362d3a243d6
ssolem <tjrals961@gmail.com>
2025-05-24 00:44:18 +0900

    [Feat] dictosave

--- Assets/01. Scripts/InGame/Manager/CurrencyManager.cs
+++ Assets/01. Scripts/InGame/Manager/CurrencyManager.cs
@@ -136,6 +136,7 @@ public class CurrencyManager : Singleton<CurrencyManager>, ISavable
             Debug.Log(CurrencySaveDic[CurrencyType.Activity]);
             CurrencySaveDic[CurrencyType.Activity] += amount;
         }
+        DicToSaveData();
     }
     public void SetCurrency(CurrencyType currency, int amount)
     {
