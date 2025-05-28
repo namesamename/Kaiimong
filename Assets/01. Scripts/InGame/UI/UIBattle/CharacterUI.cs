f54e37149393c9889c4e2ce4b744948f2cb3a39a
ssolem <tjrals961@gmail.com>
2025-05-28 23:10:31 +0900

    [Chore] Webbuild setting

--- Assets/01. Scripts/InGame/UI/UIBattle/CharacterUI.cs
+++ Assets/01. Scripts/InGame/UI/UIBattle/CharacterUI.cs
@@ -2,8 +2,6 @@ using DG.Tweening;
 using System;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
