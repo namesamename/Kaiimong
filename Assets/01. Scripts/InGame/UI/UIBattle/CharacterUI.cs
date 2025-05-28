81b7a0ea87b4759f67c7e314e10539623bb2be09
woomay0608 <woohuck0608@naver.com>
2025-05-28 21:17:15 +0900

    [feat] 모바일 빌드

--- Assets/01. Scripts/InGame/UI/UIBattle/CharacterUI.cs
+++ Assets/01. Scripts/InGame/UI/UIBattle/CharacterUI.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
+
 using UnityEngine;
 using UnityEngine.UI;
 
--- Assets/Resources/Icon/Flat Magic Spell Icons Mix Bundle1/Scenes/SetDemoImage_FlatMagicBundle1.cs
+++ Assets/Resources/Icon/Flat Magic Spell Icons Mix Bundle1/Scenes/SetDemoImage_FlatMagicBundle1.cs
@@ -13,31 +13,31 @@ namespace DemoImage_FlatMagicBundle1
         // Start is called before the first frame update
         void Start()
         {
-            DemoImage = GetComponentsInChildren<Image>();
-            for (int i = 0; i < 5; i++) // DemoImage[0] is background
-            {
-				for (int j = 1; j < 11; j++) // DemoImage[0] is background
-				{ 
-                    switch (i){
-					case 0:
-						DemoImage[i*10+j].sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Flat Magic Spell Icons Mix Bundle1/Icons/Holy/Flat Magic Spell Icons Mix Bundle1_Holy_" + j + ".png");
-						break;
-					case 1:
-						DemoImage[i*10+j].sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Flat Magic Spell Icons Mix Bundle1/Icons/Alchemy/Flat Magic Spell Icons Mix Bundle1_Alchemy_" + j + ".png");
-						break;
-					case 2:
-						DemoImage[i*10+j].sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Flat Magic Spell Icons Mix Bundle1/Icons/Fire/Flat Magic Spell Icons Mix Bundle1_Fire_" + j + ".png");
-						break;
-					case 3:
-						DemoImage[i*10+j].sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Flat Magic Spell Icons Mix Bundle1/Icons/Arcane/Flat Magic Spell Icons Mix Bundle1_Arcane_" + j + ".png");
-						break;
-					case 4:
-						DemoImage[i*10+j].sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Flat Magic Spell Icons Mix Bundle1/Icons/Nature/Flat Magic Spell Icons Mix Bundle1_Nature_" + j + ".png");
-						break;				
-					}
+   //         DemoImage = GetComponentsInChildren<Image>();
+   //         for (int i = 0; i < 5; i++) // DemoImage[0] is background
+   //         {
+			//	for (int j = 1; j < 11; j++) // DemoImage[0] is background
+			//	{ 
+   //                 switch (i){
+			//		case 0:
+			//			DemoImage[i*10+j].sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Flat Magic Spell Icons Mix Bundle1/Icons/Holy/Flat Magic Spell Icons Mix Bundle1_Holy_" + j + ".png");
+			//			break;
+			//		case 1:
+			//			DemoImage[i*10+j].sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Flat Magic Spell Icons Mix Bundle1/Icons/Alchemy/Flat Magic Spell Icons Mix Bundle1_Alchemy_" + j + ".png");
+			//			break;
+			//		case 2:
+			//			DemoImage[i*10+j].sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Flat Magic Spell Icons Mix Bundle1/Icons/Fire/Flat Magic Spell Icons Mix Bundle1_Fire_" + j + ".png");
+			//			break;
+			//		case 3:
+			//			DemoImage[i*10+j].sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Flat Magic Spell Icons Mix Bundle1/Icons/Arcane/Flat Magic Spell Icons Mix Bundle1_Arcane_" + j + ".png");
+			//			break;
+			//		case 4:
+			//			DemoImage[i*10+j].sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Flat Magic Spell Icons Mix Bundle1/Icons/Nature/Flat Magic Spell Icons Mix Bundle1_Nature_" + j + ".png");
+			//			break;				
+			//		}
 
-				}	
-			}
+			//	}	
+			//}
         }
     }
 }
